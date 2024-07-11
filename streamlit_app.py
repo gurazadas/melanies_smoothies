00c4b88c648c8060ae7c4b52c3bfe0f76bbecfb1
gurazadas <satyasai.gurazada@gmail.com>
2024-07-12 00:18:20 +0530

    Update streamlit_app.py
     Let's Put the JSON into a Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ if ingrediants_list:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #st.text(fruityvice_response.json())
- fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)       
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)       
    
